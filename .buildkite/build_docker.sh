9a24e1e5c8a523bce5b202cc0cd9a6c25a1e60fe
abizer <abizer@lambdal.com>
2023-05-26 10:50:52 -0700

    add lambda bk config

---
+++ .buildkite/build_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+REGION="us-west-2"
+REGISTRY="921575574570.dkr.ecr.us-west-2.amazonaws.com"
+PROJECT="lambda-chatbot-ui"
+BRANCH_CLEAN=${BUILDKITE_BRANCH/\//_}
+
+docker build -t "$REGISTRY/$PROJECT:${BRANCH_CLEAN}" .
+
+aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "$REGISTRY"
+
+docker push "$REGISTRY/$PROJECT:${BRANCH_CLEAN}"
